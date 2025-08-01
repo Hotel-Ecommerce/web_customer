f39ed50dea7c2952f2722750e10619f23baca356
dphuong2103 <midouz.project@gmail.com>
2025-08-01 21:41:43 +0700

    add redirects

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-let baseUrl = 'http://localhost:7079';
+let baseUrl = 'http://localhost:3000';
 
 if (process.env.NODE_ENV === 'production') {
   baseUrl = "https://hotel-api.phuongtran.site"
